31869a04a28ffb23cd3c591c853fccd8feb31024
Akash Manohar J <akash@akash.im>
2013-11-14 11:43:31 +0530

    Add a test case

--- test/realm_test.exs
+++ test/realm_test.exs
@@ -1,7 +1,14 @@
 defmodule RealmTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  defrecord Student, name: nil, age: nil, role: nil, __errors__: [] do
+    use Realm
   end
+
+  test "Get attributes" do
+    student = Student[name: "John Doe"]
+    IO.inspect student.attributes
+    assert(Student[name: "John Doe"].attributes |> Dict.get(:name) == "John Doe")
+  end
+
 end
