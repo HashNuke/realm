c6b0850edaec0a3dce1380adac5cc12014b54077
Akash Manohar J <akash@akash.im>
2013-11-16 16:18:04 +0530

    Should return attributes with strings as keys

--- lib/realm.ex
+++ lib/realm.ex
@@ -9,7 +9,7 @@ defmodule Realm do
         fields = Enum.filter @record_fields, fn({field, default_value})->
           Regex.match?(%r/\A__[a-zA-Z0-9]+__\z/, "#{field}") == false
         end
-        Enum.map fields, fn({field, default_value})-> {field, apply(record, field, [])} end
+        Enum.map fields, fn({field, default_value})-> {"#{field}", apply(record, field, [])} end
       end
 
     end
